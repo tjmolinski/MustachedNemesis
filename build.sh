fe053037874b7751b52bd8b15356c61a32c52319
TJ Molinski <tjmolinski@gmail.com>
2014-03-06 06:58:01 -0600

    Added a compile script so I don't compile the entire directory

--- build.sh
+++ build.sh
@@ -2,10 +2,18 @@
 PCZIPNAME="MustachedNemesis_PC_"
 OSXZIPNAME="MustachedNemesis_OSX_"
 DATE=$(date +%m_%d_%Y)
+echo "======================================"
 echo "Bundling up PC build"
+echo "======================================"
 cat lib/love-0.9.0-win32/love.exe MustachedNemesis.love > lib/MustachedNemesis/MustachedNemesis.exe
-zip -r bin/$PCZIPNAME$DATE lib/MustachedNemesis
+zip -9 -q -r $PCZIPNAME$DATE lib/MustachedNemesis
+mv $PCZIPNAME$DATE.zip bin
+echo "======================================"
 echo "Bundling up OSX build"
+echo "======================================"
 cp MustachedNemesis.love lib/MustachedNemesis.app/Contents/Resources/
-zip -r bin/$OSXZIPNAME$DATE lib/MustachedNemesis.app
+zip -9 -q -r $OSXZIPNAME$DATE lib/MustachedNemesis.app
+mv $OSXZIPNAME$DATE.zip bin
+echo "======================================"
 echo "Finished creating Mustached Nemesis!"
+echo "======================================"
